2f2f9173c654ef0adf57283387efc9a64a6ef20f
vikasreddynomula <billavikas.reddy@gmail.com>
2025-11-14 15:18:32 -0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+//website developed by Vikas Reddy Nomula
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
